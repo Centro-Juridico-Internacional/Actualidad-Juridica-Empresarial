e1c1e43968c48bd6569b2eac4ebe285cf19d574c
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-08-22 15:19:58 -0500

    fix: eliminar consola de depuración en la función getProducts

--- lib/get-products.ts
+++ lib/get-products.ts
@@ -7,7 +7,6 @@ export function getProducts({ categoryId }: { categoryId: string }) {
 		`products?filters[product_category][slug][$contains]=${categoryId}&populate=images`
 	).then((res) => {
 		const { data, meta } = res;
-		console.log(data);
 		const products = data.map((i: any) => {
 			const { name, slug, description, images, price, stock } = i;
 			const image = `${STRAPI_HOST}${images?.[0]?.url}`;
