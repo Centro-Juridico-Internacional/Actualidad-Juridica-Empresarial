8b619cefe54ba8bf256c3c9822e3b04b37787093
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-10-29 11:45:15 -0500

    feat: refactor funciones para incluir manejo de URLs con host en getAuthors, getBanner, getCategories, getInterviewsUrl, getNews y getPautas

--- src/lib/get-authors.ts
+++ src/lib/get-authors.ts
@@ -1,17 +1,16 @@
-import { query } from './strapi';
+import { query, withHost } from './strapi';
 
-const { STRAPI_HOST } = process.env;
+export async function getAuthors() {
+	const res = await query('authors?fields[0]=name&populate[avatar][fields][0]=url');
+	// Strapi v4/v5: res.data = [{ id, attributes: { name, avatar: { data: { attributes: { url }}}}}]
+	return res.data.map((item: any) => {
+		const a = item.attributes ?? item; // fallback por si viene aplanado
+		const name = a.name;
+		const avatarRel = a?.avatar?.data?.attributes?.url ?? a?.avatar?.url ?? null;
 
-export function getAuthors() {
-	return query('authors?fields[0]=name&populate[avatar][fields][0]=url').then((res) => {
-		return res.data.map((author: any) => {
-			const { name, avatar } = author;
-			const avatarUrl = avatar?.url ? `${STRAPI_HOST}${avatar.url}` : null;
-
-			return {
-				name,
-				avatar: avatarUrl
-			};
-		});
+		return {
+			name,
+			avatar: withHost(avatarRel)
+		};
 	});
 }
--- src/lib/get-interviewsUrl.ts
+++ src/lib/get-interviewsUrl.ts
@@ -1,16 +1,14 @@
 import { query } from './strapi';
 
-export function getInterviewsUrl() {
-	return query('entrevistas-urls?fields[0]=UrlEntrevista&fields[1]=Titulo&sort=updatedAt:asc').then(
-		(res) => {
-			return res.data.map((interview: any) => {
-				const { UrlEntrevista, Titulo } = interview;
-
-				return {
-					url: UrlEntrevista || null,
-					title: Titulo || null
-				};
-			});
-		}
+export async function getInterviewsUrl() {
+	const res = await query(
+		'entrevistas-urls?fields[0]=UrlEntrevista&fields[1]=Titulo&sort=updatedAt:asc'
 	);
+	return res.data.map((item: any) => {
+		const a = item.attributes ?? item;
+		return {
+			url: a?.UrlEntrevista ?? null,
+			title: a?.Titulo ?? null
+		};
+	});
 }
