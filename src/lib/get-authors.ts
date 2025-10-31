79ba7b30e4920ac174f0c451a313024f95b9b192
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-10-31 16:19:07 -0500

    autores

--- src/lib/get-authors.ts
+++ src/lib/get-authors.ts
@@ -1,15 +1,18 @@
+// src/lib/get-authors.ts
 import { query, withHost } from './strapi';
 
 export async function getAuthors() {
-	const res = await query('authors?fields[0]=name&populate[avatar][fields][0]=url');
-	// Strapi v4/v5: res.data = [{ id, attributes: { name, avatar: { data: { attributes: { url }}}}}]
-	return res.data.map((item: any) => {
-		const a = item.attributes ?? item; // fallback por si viene aplanado
-		const name = a.name;
-		const avatarRel = a?.avatar?.data?.attributes?.url ?? a?.avatar?.url ?? null;
+	// Strapi v4/v5: /api/authors?fields[0]=name&fields[1]=role&populate[avatar][fields][0]=url
+	const res = await query('authors?fields[0]=name&fields[1]=role&populate[avatar][fields][0]=url');
 
+	return res.data.map((item: any) => {
+		const a = item.attributes ?? item; // v4/v5 compat
+		const name = a.name;
+		const role = a.role ?? null;
+		const avatarRel = a?.avatar?.data?.attributes?.url ?? a?.avatar?.url ?? null;
 		return {
 			name,
+			role,
 			avatar: withHost(avatarRel)
 		};
 	});
