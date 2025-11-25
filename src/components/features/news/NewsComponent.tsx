0f7b7b74e40780b1d70df2bcd90cede1a79637df
Desarrollo 1 <desarrollo1@centrojuridicointernacional.com>
2025-11-25 15:30:39 -0500

    feat: Add NewsComponent, integrate it into a homepage news section, and create a dedicated news search page.

--- src/components/features/news/NewsComponent.tsx
+++ src/components/features/news/NewsComponent.tsx
@@ -96,6 +96,7 @@ const NewsComponent: React.FC<NewsComponentProps> = ({
 								isCompact ? 'prose-sm line-clamp-2' : 'prose-base md:line-clamp-3'
 							}`}
 						>
+							{/* @ts-ignore */}
 							<BlocksRenderer content={blocks} />
 						</div>
 
